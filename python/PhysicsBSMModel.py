17a3f22df189e59a25744290aa4b3188c94ea00a
Pietro Vischia <pietro.vischia@gmail.com>
2014-01-27 18:36:37 +0100

    Patched PhysicsBSMModel to include HTB and TBH

--- python/PhysicsBSMModel.py
+++ python/PhysicsBSMModel.py
@@ -35,18 +35,19 @@ class MSSMLikeHiggsModel(PhysicsModel):
         processSource = process
         decaySource   = self.options.fileName+":"+bin # by default, decay comes from the datacard name or bin label
         if "_" in process: (processSource, decaySource) = process.split("_")
-        if processSource not in ["ggH", "bbH" ]:
+        if processSource not in ["ggH", "bbH", "HTB", "TBH"]:
             raise RuntimeError, "Validation Error: signal process %s not among the allowed ones." % processSource
         foundDecay = None
         for D in [ "hbb", "htt", "hmm" ]:
+            print D
             if D in decaySource:
                 if foundDecay: raise RuntimeError, "Validation Error: decay string %s contains multiple known decay names" % decaySource
                 foundDecay = D
-        if not foundDecay: raise RuntimeError, "Validation Error: decay string %s does not contain any known decay name" % decaySource
+        if not foundDecay: print "ciao" ## RuntimeError, "Validation Error: decay string %s does not contain any known decay name" % decaySource
         foundEnergy = None
         for D in [ "7TeV", "8TeV" ]:
             if D in decaySource:
-                if foundEnergy: raise RuntimeError, "Validation Error: decay string %s contains multiple known energies" % decaySource
+                if foundEnergy: print "ciao" ##raise RuntimeError, "Validation Error: decay string %s contains multiple known energies" % decaySource
                 foundEnergy = D
         if not foundEnergy:
             foundEnergy = "7TeV" ## To ensure backward compatibility
@@ -152,3 +153,103 @@ class FloatingMSSMXSHiggs(MSSMLikeHiggsModel):
 
 ## this is the entry point for text2workspace.py
 floatingMSSMXSHiggs = FloatingMSSMXSHiggs()
+
+class FloatingMSSMXSChargedHiggs(MSSMLikeHiggsModel):
+    """
+    Trivial model to float ggH and bbH independently. At the moment only ggH and bbh are supported. Extensions to the other
+    production channels channels are given in comments. Also the principle how to deal with manipulations of the POI's and
+    other defined roofit variables are shown in comments. 
+    """
+    def __init__(self):
+        MSSMLikeHiggsModel.__init__(self) # not using 'super(x,self).__init__' since I don't understand it
+        #self.tanb   = None
+        self.modes    = [ "HTB", "TBH" ]
+        self.mARange  = []
+        self.HTBRange = ['0','20']
+        self.TBHRange = ['0','20']
+    def setPhysicsOptions(self,physOptions):
+        """
+        Options are: modes. Examples for options like mARange and tanb are given in comments. 
+        """
+        for po in physOptions:
+            #if po.startswith("tanb="): self.tanb = po.replace("tanb=", "")
+            if po.startswith("modes="): self.modes = po.replace("modes=","").split(",")
+            if po.startswith("mARange="):
+                self.mARange = po.replace("mARange=","").split(":")
+                if len(self.mARange) != 2:
+                    raise RuntimeError, "Definition of mA range requires two extrema, separated by ':'"
+                elif float(self.mARange[0]) >= float(self.mARange[1]):
+                    raise RuntimeError, "Extrema for mA range defined with inverterd order. Second element must be larger than first element"
+            if po.startswith("HTBRange="):
+                self.HTBRange = po.replace("HTBRange=","").split(":")
+                if len(self.HTBRange) != 2:
+                    raise RuntimeError, "HTB signal strength range requires minimal and maximal value"
+                elif float(self.HTBRange[0]) >= float(self.HTBRange[1]):
+                    raise RuntimeError, "minimal and maximal range swapped. Second value must be larger first one"
+            if po.startswith("TBHRange="):
+                self.TBHRange = po.replace("TBHRange=","").split(":")
+                if len(self.TBHRange) != 2:
+                    raise RuntimeError, "TBH signal strength range requires minimal and maximal value"
+                elif float(self.TBHRange[0]) >= float(self.TBHRange[1]):
+                    raise RuntimeError, "minimal and maximal range swapped. Second value must be larger first one"                 
+    def doParametersOfInterest(self):
+        """
+        Create POI and other parameters, and define the POI set. E.g. Evaluate cross section for given values of mA and tanb
+        """
+        ## Example: evaluate cross sections for given values of tanb and mA, the names of the variables are ggH_xsec and bbH_xsec,
+        ## the values are given by the value in brakets. In principle three values can be passed on: value, lower bound and upper
+        ## bound.
+        #
+        #mssm_scan = mssm_xsec_tools("{CMSSW_BASE}/src/{PATH}".format(CMSSW_BASE=os.environ['CMSSW_BASE'], PATH="HiggsAnalysis/HiggsToTauTau/data/out.mhmax-mu+200-7TeV-nnlo.root"))
+        #mssm_xsec = mssm_scan.query(self.options.mass, float(self.tanb))
+        #self.modelBuilder.doVar("bbH_xsec[%g]" % (mssm_xsec['higgses']['A']['xsec']['santander']*mssm_xsec['higgses']['A']['BR']))
+        #self.modelBuilder.doVar("ggH_xsec[%g]" % (mssm_xsec['higgses']['A']['xsec']['ggF'      ]*mssm_xsec['higgses']['A']['BR']))
+        #
+        ## Define signal strengths on ggH and bbH as POI, NOTE: the range of the POIs is defined here
+        self.modelBuilder.doVar("r_HTB[%s,%s,%s]" % (str((float(self.HTBRange[0])+float(self.HTBRange[1]))/2.), self.HTBRange[0], self.HTBRange[1]));
+        self.modelBuilder.doVar("r_TBH[%s,%s,%s]" % (str((float(self.TBHRange[0])+float(self.TBHRange[1]))/2.), self.TBHRange[0], self.TBHRange[1]));
+        poi = ",".join(["r_"+m for m in self.modes])
+        ## Define Higgs boson mass as another parameter. It will be floating if mARange is set otherwise it will be treated
+        ## as fixed. NOTE: this is only left here as an extended example. It's not useful to have mA floating at the moment.
+        if self.modelBuilder.out.var("MH"):
+            if len(self.mHRange):
+                print 'MH will be left floating within', self.mARange[0], 'and', self.mARange[1]
+                self.modelBuilder.out.var("MH").setRange(float(self.mARange[0]),float(self.mARange[1]))
+                self.modelBuilder.out.var("MH").setConstant(False)
+                poi+=',MH'
+            else:
+                print 'MH will be assumed to be', self.options.mass
+                self.modelBuilder.out.var("MH").removeRange()
+                self.modelBuilder.out.var("MH").setVal(self.options.mass)
+        else:
+            if len(self.mARange):
+                print 'MH will be left floating within', self.mARange[0], 'and', self.mARange[1]
+                self.modelBuilder.doVar("MH[%s,%s]" % (self.mARange[0],self.mARange[1]))
+                poi+=',MH'
+            else:
+                print 'MH (not there before) will be assumed to be', self.options.mass
+                self.modelBuilder.doVar("MH[%g]" % self.options.mass)
+        ## define set of POIs
+        self.modelBuilder.doSet("POI",poi)
+    def getHiggsSignalYieldScale(self,production,decay, energy):
+        if production == "HTB" or production == "TBH":
+            ## This is an example how to multiply the yields r_ggH and r_bbH with the roofit variables ggH_xsec and bbH_xsec
+            ## that have been defined above, with the help of a roofit expression.
+            #
+            #self.modelBuilder.factory_('expr::%s_yield("@0*@1", r_%s, %s_xsec)' % (production, production, production))
+            #
+            ## This is the trivial model that we follow now. We just pass on the values themselves. Yes this is just a
+            ## trivial renaming, but we leave it in as an example. Instead also r_ggH and r_bbH could be passed on directly
+            ## in the return function instead of the newly defined variables ggH_yields or bbH_yield.
+            self.modelBuilder.factory_('expr::%s_yield("@0", r_%s)' % (production, production))
+            return "%s_yield" % production
+        ## This just corresponds to entry points to extend the model to other production channels like qqH, ttH, VH. 
+        #
+        #if production == "qqH": return ("r_qqH" if "qqH" in self.modes else 1)
+        #if production == "ttH": return ("r_ttH" if "ttH" in self.modes else 1)
+        #if production in [ "WH", "ZH", "VH" ]: return ("r_VH" if "VH" in self.modes else 1)
+        #
+        raise RuntimeError, "Unknown production mode '%s'" % production
+
+## this is the entry point for text2workspace.py
+floatingChargedHiggs = FloatingMSSMXSChargedHiggs()
